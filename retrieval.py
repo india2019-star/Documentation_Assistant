dc53c1775b09bf2745b05797fab8ef36c6ad3214
Raj Mitra <indiahind320@gmail.com>
2024-09-20 11:24:04 +0530

    Added Score Threshold for better filtering

--- retrieval.py
+++ retrieval.py
@@ -3,6 +3,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_community.vectorstores import PGVector
+from langchain.prompts import PromptTemplate
 from langchain import hub
 from typing import List, Dict
 import os
@@ -18,11 +19,29 @@ def retrieval_func(question, chat_history: List[Dict[str, str]] = []):
     vector_store = PGVector(connection_string=os.environ['POSTGRE_CONNECTION_STRING'],
                             collection_name=os.environ['POSTGRE_COLLECTION_NAME'],
                             embedding_function=embeddings)
+    # template = r'''
+    # Answer the questions based solely on the context provided below:
+    # <context>
+    # {context}
+    # <context>
+
+    # Chat History:
+    # {chat_history}
+
+    # Human:
+    # {input}
+    # If an answer cannot be formulated from the provided context DO NOT ATTEMPT TO ANSWER.
+    # '''
+
+    
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
     combine_stuff_documents = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
     history_chat_prompt = hub.pull("langchain-ai/chat-langchain-rephrase")
     history_retrieval_chat = create_history_aware_retriever(
-        llm, vector_store.as_retriever(search_kwargs={"k": 2}), history_chat_prompt
+        llm, vector_store.as_retriever(search_type="similarity_score_threshold",search_kwargs={
+                                                                                                "k": 5,
+                                                                                                "score_threshold": 0.61}),
+                                                                                                  history_chat_prompt
     )
     retrieval_chain = create_retrieval_chain(
         history_retrieval_chat, combine_docs_chain=combine_stuff_documents
